78f3963d260297f4c269042713d5271fb01e0c93
Yufang <wangalisa2017@gmail.com>
2024-12-08 18:14:38 +0100

    using continous number information

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 import numpy as np
 from scipy.sparse import coo_matrix, save_npz, load_npz
 import hashlib
 from NetflixSimiarlity import NetflixSimiarlity
 import time
+from scipy.sparse import csr_matrix, csc_matrix
 
 
 def create_user_movie_matrix(data):
@@ -15,8 +15,8 @@ def create_user_movie_matrix(data):
     else:
         print("The array does not contain any NaN values.")
     
-    userlen=len(np.unique(data[:,0])) + 1
-    movielen=len(np.unique(data[:,1])) + 1
+    userlen= np.max(data[:,0]) + 1
+    movielen= np.max(data[:,1]) + 1
     # print(userlen,movielen)
     user = data[:,0]
     movie = data[:,1]
@@ -27,21 +27,6 @@ def create_user_movie_matrix(data):
     save_npz("user_movie.npz", user_movie)
     return user_movie
 
-# convert  value in every row to a 128 array ,then get a signature matrix
-# def minhash(data,seed):
-#     similaruser=[]
-
-#     for row in data:
-#         m=Minhash(seed=seed)
-#         # upodate hasevalue in one row
-#         for index in row.indices:
-#             m.update(str(index).encode('utf-8'))
-#         similaruser.append(m)
-#     return similaruser
-
-#  a b
-# a[1,2
-# b 1,2]
 
 if __name__ == '__main__':
     np.random.seed(123456)
@@ -52,18 +37,18 @@ if __name__ == '__main__':
     # Now you can save it or perform operations as needed
     # user_movie = load_npz("./user_movie.npz")
 
-    print("this is my user_movie_matrix")
-    print(user_movie_matrix)
+    print("user_movie_matrix has been obtained")
     movie_user_matrix = user_movie_matrix.T
-    del data, user_movie, user_movie_matrix # delet the unnecessary variables to save memory
-    NetflixSimiarlity_user = NetflixSimiarlity(movie_user_matrix)
-    NetflixSimiarlity_user.create_signature_matrix_sparse_parallel(num_permutations = 1000)
+
+
+    movie_user_sparse = csr_matrix(movie_user_matrix) # # Sparse matrix in CSR format
+    del data, user_movie, user_movie_matrix, movie_user_matrix # delet the unnecessary variables to save memory
+    NetflixSimiarlity_user = NetflixSimiarlity(movie_user_sparse)
+    NetflixSimiarlity_user.create_signature_matrix_sparse_parallel(num_permutations = 100)
     NetflixSimiarlity_user.bands_hashing(bandNum=1)
-    print(NetflixSimiarlity_user.candidate_pairs)
+    #print(NetflixSimiarlity_user.candidate_pairs)
     filtered_Jaccard = NetflixSimiarlity_user.Jaccard_simiarlity(threshold = 0.5)
     end_time = time.time()
     print(filtered_Jaccard)
     execution_time = end_time - start_time
     print(f"Execution Time: {execution_time:.4f} seconds")
-
-    
